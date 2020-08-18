601360e1bf91cb8835b26a8f59be73ce9b465072
root <zaid.a530@gmail.com>
2020-08-18 19:15:38 +0530

    18_08_2020-auth-user-changes

--- controllers/auth_user.controller.js
+++ controllers/auth_user.controller.js
@@ -2,8 +2,7 @@
 
 // Importing required modules.
 const authUserModel = require('../models/auth_user.model');
-const userHelper = require('../helpers/user_methods');
-const decryptPassHelper = require('../helpers/decrypt_password');
+
 
 /****************************************************
  * Register user  API.                              *
@@ -22,26 +21,14 @@ exports.register = async (req, res) => {
             phone: phone,
             isAdmin: isAdmin
         });
-
-        // checks whether the email is already registered or not.
-        await userHelper.findUser(username)
-            .then(async (userDetails) => {
-                if (userDetails !== "false") return res.status(201).json({ status: 201, message: "Username already exists" });
-                // If not registered then create.
-                else return await userHelper.createUser(userModelObj)
-                    .then((createdUser) => {
-                        return res.status(200).json({ status: 201, message: "Registered Successfully", data: createdUser });
-                    })
-                    .catch((err) => {
-                        return res.status(401).json({ status: 401, message: "Something wents wrong", error: err });
-                    });
-            })
-            .catch((err) => {
-                return res.status(401).json({ status: 401, message: "Something wents wrong", error: err });
-            });
+        userModelObj.save(userModelObj,(error,createdUser)=>{
+            if(error) return res.status(401).json({ status: 401, message: "Check all your fields", error: error });
+            else if (!createdUser['id']) return res.status(401).json({ status: 401, message: "User registration unsuccesssfull", data: createdUser });
+            else return res.status(200).json({ status: 401, message: "User registered", data: createdUser });
+        })
     }
     catch (error) {
-        return res.status(401).json({ status: 401, message: "Something wents wrong", error: err });
+        return res.status(401).json({ status: 401, message: "Something wents wrong", error: error });
     }
 }
 /****************************************END*************************************/
